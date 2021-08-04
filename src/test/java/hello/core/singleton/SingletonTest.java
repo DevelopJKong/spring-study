2a85a6b64f2f28b6c5e724ee6d7f58aae2e3ad5e
DevelopJKong <ybaram40@gmail.com>
2021-08-04 12:35:44 +0900

    4주차 32강 완료

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     @Test
@@ -23,9 +25,22 @@ public class SingletonTest {
         System.out.println("memberService2 = "+ memberService2);
 
         //memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
-
-
-
+        assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = "+ singletonService1);
+        System.out.println("singletonService2 = "+ singletonService2);
+
+        //isEqualTo vs isSameAs 
+        //same은 타입은 아니고 그 값이 같은지
+        //equal는 다 비교
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
 }
