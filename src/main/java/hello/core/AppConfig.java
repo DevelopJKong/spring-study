7e9d766898257b8c2babf27e84bb4d92ac9ab817
DevelopJKong <ybaram40@gmail.com>
2021-08-07 22:43:15 +0900

    3주차 37강 완료

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -13,18 +13,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemberyMemberRepository()
+
+
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+    //순서는 보장하지 않는다 하지만 이렇게 5개는 출력 되어야 한다
+
+    //실제 테스트 상황
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+
     @Bean
     public MemberService memberService(){
+
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepositroy memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepositroy();
     }
 
     @Bean
     public OrderService orderService(){
+
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
