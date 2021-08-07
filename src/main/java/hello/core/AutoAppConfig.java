ee99405f5489e745f8da261c9aaf4767a76b6463
DevelopJKong <ybaram40@gmail.com>
2021-08-08 00:30:27 +0900

    3주차 38강 완료

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+//@Component로 설정한것을 자동으로 스프링 빈으로 등록해준다
+@ComponentScan (
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION,classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
--- src/main/java/hello/core/member/MemoryMemberRepositroy.java
+++ src/main/java/hello/core/member/MemoryMemberRepositroy.java
@@ -1,7 +1,12 @@
 package hello.core.member;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+
+@Component
 public class MemoryMemberRepositroy implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<Long,Member>();
