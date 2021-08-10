cd51da53be817ce2323161090bf92934de4e03e5
DevelopJKong <ybaram40@gmail.com>
2021-08-10 12:43:25 +0900

    39강 완료

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 //@Component로 설정한것을 자동으로 스프링 빈으로 등록해준다
 @ComponentScan (
+        basePackages ="hello.core.member",
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION,classes = Configuration.class)
 )
 public class AutoAppConfig {
