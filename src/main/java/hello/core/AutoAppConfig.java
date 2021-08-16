f4daca01255efd7334c0e718f914769b13d65581
DevelopJKong <ybaram40@gmail.com>
2021-08-16 19:36:34 +0900

    48강 에러 해결 완료

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 //@Component로 설정한것을 자동으로 스프링 빈으로 등록해준다
 @ComponentScan (
-        basePackages ="hello.core.member",
+        //basePackages ="hello.core.member", -> 이것을 주석 처리 하지 않으면 nullpointexception이 발생한다
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION,classes = Configuration.class)
 )
 public class AutoAppConfig {
